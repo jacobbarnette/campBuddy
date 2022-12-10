5689cec0b4dff0e971f12b073db0bf6acb1490b9
jacobbarnette <jacobcbarnette@gmail.com>
2022-12-10 17:11:40 -0500

    added user authentication

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const connectDB = require("./config/db");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", require("./routes/userRoutes"));
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
-
-app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(port, () => {
   console.log(`app is listening on ${port}`);
