feedc9f6847ff7c7446598d3502c171701cc1ff8
jacobbarnette <jacobcbarnette@gmail.com>
2022-12-10 19:01:44 -0500

    created login and register pages

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,29 @@
-import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Form className="form">
+      <Form.Group className="email ">
+        <Form.Label>Email address:</Form.Label>
+        <Form.Control type="email" placeholder="enter email" />
+      </Form.Group>
+      <Form.Group className="password ">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" placeholder="enter password" />
+      </Form.Group>
+      <br />
+      <Button
+        style={{ width: "100%" }}
+        classname="loginBtn"
+        size="lg"
+        variant="primary"
+        type="submit"
+      >
+        Login
+      </Button>
+    </Form>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,37 @@
-import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Form className="form">
+      <Form.Group className="name ">
+        <Form.Label>Name:</Form.Label>
+        <Form.Control type="name" placeholder="enter name" />
+      </Form.Group>
+      <Form.Group className="email ">
+        <Form.Label>Email address:</Form.Label>
+        <Form.Control type="email" placeholder="enter email" />
+      </Form.Group>
+      <Form.Group className="password ">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" placeholder="enter password" />
+      </Form.Group>
+      <Form.Group className="password ">
+        <Form.Label> Confirm Password:</Form.Label>
+        <Form.Control type="password" placeholder="confirm password" />
+      </Form.Group>
+      <br />
+      <Button
+        style={{ width: "100%" }}
+        classname="registerBtn"
+        size="lg"
+        variant="primary"
+        type="submit"
+      >
+        Register
+      </Button>
+    </Form>
+  );
 };
 
 export default Register;
