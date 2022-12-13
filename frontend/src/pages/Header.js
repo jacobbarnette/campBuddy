a3eceb5c8d783bbd31ed3369f320bec613b94b01
jacobbarnette <jacobcbarnette@gmail.com>
2022-12-13 12:43:18 -0500

    Update Header.js

--- frontend/src/pages/Header.js
+++ frontend/src/pages/Header.js
@@ -3,6 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { logout, reset } from "../features/auth/authSlice";
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
@@ -23,18 +24,36 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link>Campgrounds</Nav.Link>
+              <Nav.Link>Add New</Nav.Link>
+            </Nav>
             <Nav className="ms-auto">
               {user ? (
-                <LinkContainer to="/">
-                  <Nav.Link onClick={onLogout}>Logout</Nav.Link>
-                </LinkContainer>
+                <>
+                  <Nav.Link>Signed in as {user.name}</Nav.Link>
+                  <LinkContainer to="/">
+                    <Nav.Link onClick={onLogout}>
+                      {" "}
+                      <FaSignOutAlt />
+                      Logout
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
               ) : (
                 <>
                   <LinkContainer to="/login">
-                    <Nav.Link>Login</Nav.Link>
+                    <Nav.Link>
+                      {" "}
+                      <FaSignInAlt /> Login
+                    </Nav.Link>
                   </LinkContainer>
                   <LinkContainer to="/register">
-                    <Nav.Link>Register</Nav.Link>
+                    <Nav.Link>
+                      {" "}
+                      <FaUser />
+                      Register
+                    </Nav.Link>
                   </LinkContainer>
                 </>
               )}
