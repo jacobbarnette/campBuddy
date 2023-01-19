60181302b17c3457e45dcee436c9fefe0a3791bd
jacobbarnette <jacobcbarnette@gmail.com>
2023-01-19 14:39:32 -0500

    new vercel.json

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.static("frontend/src"));
 app.use(cors());
 
 app.use("/api/users", require("../routes/userRoutes"));
