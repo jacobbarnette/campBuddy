99d4ee75791c6e1c1bf3999b684637d3617483eb
jacobbarnette <jacobcbarnette@gmail.com>
2023-01-21 18:54:52 -0500

    removed sendStatus

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("frontend"));
 app.use(cors());
-console.log(MONGO_URI);
+
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/campgrounds", require("./routes/campRoutes"));
 
 connectDB();
 
 app.get("/", (req, res) => {
-  res.sendStatus(200);
   res.send("hello");
 });
 app.listen(port, () => {
