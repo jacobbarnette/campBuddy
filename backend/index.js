ca8cdcd888b37f178f0cc00f5a8aa88c882b1156
jacobbarnette <jacobcbarnette@gmail.com>
2023-01-21 18:25:59 -0500

    added console.log for mongo_uri

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 const connectDB = require("./config/db");
 const cors = require("cors");
 
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("frontend"));
 app.use(cors());
-
+console.log(MONGO_URI);
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/campgrounds", require("./routes/campRoutes"));
 
