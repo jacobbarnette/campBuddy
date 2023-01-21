230c8ffd9a7a6f12f778e5798ec9923916bf7c05
jacobbarnette <jacobcbarnette@gmail.com>
2023-01-21 14:38:12 -0500

    test

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("frontend/src"));
+app.use(express.static("frontend"));
 app.use(cors());
 
 app.use("/api/users", require("../routes/userRoutes"));
