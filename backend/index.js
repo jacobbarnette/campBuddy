9d0d18695d459de757c0d33a51bbefd5270e9fff
jacobbarnette <jacobcbarnette@gmail.com>
2023-01-22 13:34:48 -0500

    created build dir from frontend

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ const port = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const connectDB = require("./config/db");
 const cors = require("cors");
-
+const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("frontend"));
 app.use(cors());
+
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/campgrounds", require("./routes/campRoutes"));
