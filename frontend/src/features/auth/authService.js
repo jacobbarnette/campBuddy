53457815ca64db9e9444c5ce8ab05444370506eb
jacobbarnette <jacobcbarnette@gmail.com>
2023-01-23 20:12:24 -0500

    changed base_url var

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api/users/";
+const BASE_URL = "https://campbuddy.onrender.com/api/users/";
 
 const register = async (userData) => {
   const response = await axios.post(BASE_URL, userData);
